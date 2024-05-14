4c2696c3fa1bbf93795778c45c3a327bed521505
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-05-14 13:10:52 -0400

    att

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ res.send('Hello World')
 app.get('/oi', function(req,res){
   res.send('Olá Mundo!!')
 })
+// List de itens
+//Endpoint de Read All [GET] /item
+const itens = [' Rick Sanchez', ' Morty Smith', 'Summer Smith']
+app.get('/item', function (req, res){
+  res.send(itens)
+})
 app.listen(3000)
