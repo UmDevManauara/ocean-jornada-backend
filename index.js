17ca16814bf0455b0678594d7e3e495bf80e635f
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-05-14 10:51:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
+
+app.get('/', function (req, res) {
+res.send('Hello World')
+})
+//desafio
 app.get('/oi', function(req,res){
   res.send('Olá Mundo')
 })
 app.listen(3000)
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
-
-// app.listen(3000)
