19d0600dea5ac840b19ea059250b95808f215015
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-05-14 13:30:32 -0400

    att

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ app.get('/oi', function(req,res){
 // List de itens
 //Endpoint de Read All [GET] /item
 const itens = [' Rick Sanchez', ' Morty Smith', 'Summer Smith']
+// 0   1  2
 app.get('/item', function (req, res){
   res.send(itens)
+})
+// Endpoit Read Single by ID / item/:id
+app.get('/item/:id', function (req, res){
+  //acessa o parâmetro de rota ID
+  const id = req.params.id
+
+// acessa o item na lista usando ID - 1
+const item = itens[id - 1]
+//enviamos o item encontrado como resposta
+  res.send(item)
 })
 app.listen(3000)
